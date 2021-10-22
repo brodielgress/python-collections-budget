8b4eba5326818dc7661bb89bc0220f32871bd90b
Brodie Gress <77457950+brodielgress@users.noreply.github.com>
2021-10-22 19:05:54 -0400

    Maybe this will make it work

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -2,9 +2,9 @@ from . import Expense
 
 import collections
 
-expenses = Expense.Expenses(data/spending_data.csv)
+expenses.read_expenses(data/spending_data.csv) = Expense.Expenses() 
+
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
     spending_counter = collections.Counter(spending_categories)
-    
